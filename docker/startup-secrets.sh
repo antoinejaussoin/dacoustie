bba131efab38936dc556d2edfbb219b2ddedc2e5
Antoine Jaussoin <antoine@jaussoin.com>
2021-12-30 17:25:54 +0100

    https wip

--- docker/startup-secrets.sh
+++ docker/startup-secrets.sh
@@ -8,5 +8,6 @@ DB_USER="${DB_USER:-dac}" \
 DB_PASSWORD="${DB_PASSWORD:-changeme}" \
 DB_NAME="${DB_NAME:-dacoustie}" \
 SECRET="${SECRET:-changeme}" \
-envsubst '${FTP_USER} ${FTP_PASSWORD} ${DB_HOST} ${DB_USER} ${DB_PASSWORD} ${DB_NAME} ${SECRET}' < /var/www/dacoustie/configuration.php.template > /var/www/dacoustie/configuration.php
+BASE_URL="${BASE_URL:-foo}" \
+envsubst '${FTP_USER} ${FTP_PASSWORD} ${DB_HOST} ${DB_USER} ${DB_PASSWORD} ${DB_NAME} ${SECRET} ${BASE_URL}' < /var/www/dacoustie/configuration.php.template > /var/www/dacoustie/configuration.php
 
